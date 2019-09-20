2cf0d7f6ccea0f9987ac0cfe54b91541be98c1ff
Romik231 <Romik231@yandex.ru>
2019-09-20 15:40:21 +0300

    Api на минималках, пока получение всех пользователей, и по конкретному id

---
+++ tps-basic/web/app/views/users/_form.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Users */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="users-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'password_hash')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'token')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'firstname')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'lastname')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'surname')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'phoneNumber')->textInput() ?>
+
+    <?= $form->field($model, 'createAt')->textInput() ?>
+
+    <?= $form->field($model, 'updateAt')->textInput() ?>
+
+    <?= $form->field($model, 'status')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
